186d11b2c944bea3211f10adea65b6553f8cdd09
AyushUnleashed <myzealouslifeofficial@gmail.com>
2023-02-23 04:13:54 +0530

    First Build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './sample_images/logo.png';
+import image from './sample_images/ai_cover.jpg';
+import photoshoot_1 from './sample_images/photoshoot_1.png';
+import photoshoot_2 from './sample_images/photoshoot_2.png';
+import photoshoot_3 from './sample_images/photoshoot_3.png';
+import photoshoot_4 from './sample_images/photoshoot_4.png';
+import photoshoot_5 from './sample_images/photoshoot_5.png';
+import photoshoot_6 from './sample_images/photoshoot_6.png';
 import './App.css';
 
 function App() {
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalImage, setModalImage] = useState(null);
+
+  const openModal = (image) => {
+    setModalOpen(true);
+    setModalImage(image);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setModalImage(null);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="logo-container">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">PhotoSnap AI</h1>
+        </div>
       </header>
+      <main className="App-main">
+        <img src={image} className="main-image" alt="AI Related" />
+        <h2 className="main-headline">Transforming Your Vision into Stunning Reality with ProSnapAI</h2>
+        <p className="main-subheadline">Get professional-grade pictures that are visually stunning and tailored to your style and preferences.</p>
+        <div className="examples-container">
+          <img src={photoshoot_1} alt="Example 1" onClick={() => openModal(photoshoot_1)}/>
+          <img src={photoshoot_2} alt="Example 2" onClick={() => openModal(photoshoot_2)}/>
+          <img src={photoshoot_3} alt="Example 3" onClick={() => openModal(photoshoot_3)}/>
+          <img src={photoshoot_4} alt="Example 4" onClick={() => openModal(photoshoot_4)}/>
+          <img src={photoshoot_5} alt="Example 5" onClick={() => openModal(photoshoot_5)}/>
+          <img src={photoshoot_6} alt="Example 6" onClick={() => openModal(photoshoot_6)}/>
+        </div>
+        {modalOpen && (
+          <div className="modal-container" onClick={closeModal}>
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+              <img src={modalImage} className="modal-image" alt="Full size" />
+            </div>
+          </div>
+        )}
+        <a href="https://wa.me/whatsapp_number" target="_blank" rel="noopener noreferrer" className="contact-button">Contact Now</a>
+      </main>
+      <footer className="App-footer">
+        <p className="footer-text">Who made this?</p>
+        <a href="https://www.linkedin.com/in/name" target="_blank" rel="noopener noreferrer" className="linkedin-link">LinkedIn</a>
+      </footer>
     </div>
   );
 }
